ee53924f5dd4dad2834d8307dc51f485955da371
Philpax <me@philpax.me>
2024-04-19 00:26:54 +0200

    feat: move strobing to a button

--- static/font-picker.js
+++ static/font-picker.js
@@ -51,29 +51,47 @@ link.href = `https://fonts.googleapis.com/css2?family=${families}&display=swap`;
 link.rel = "stylesheet";
 head.appendChild(link);
 
-document.body.onload = () => {
-  let body = document.body;
-  let fontPicker = document.createElement("select");
-  candidates.forEach((font) => {
-    let option = document.createElement("option");
-    option.value = font;
-    option.innerText = font;
-    fontPicker.appendChild(option);
-  });
-  fontPicker.addEventListener("change", function () {
-    body.style.fontFamily = fontPicker.value;
-  });
+let body = document.body;
+let fontPicker = document.createElement("select");
+candidates.forEach((font) => {
+  let option = document.createElement("option");
+  option.value = font;
+  option.innerText = font;
+  fontPicker.appendChild(option);
+});
+fontPicker.addEventListener("change", function () {
   body.style.fontFamily = fontPicker.value;
-  body.prepend(fontPicker);
-};
+});
+body.style.fontFamily = fontPicker.value;
+body.prepend(fontPicker);
 
-setInterval(() => {
-  let isLight = document.documentElement.classList.contains("light-theme");
-  if (isLight) {
-    document.documentElement.classList.remove("light-theme");
-    document.documentElement.classList.add("dark-theme");
+let isStrobing = false;
+
+const strobeButton = document.createElement("button");
+strobeButton.innerText = "Toggle Strobing";
+strobeButton.addEventListener("click", function () {
+  isStrobing = !isStrobing;
+  if (isStrobing) {
+    startStrobing();
   } else {
-    document.documentElement.classList.remove("dark-theme");
-    document.documentElement.classList.add("light-theme");
+    stopStrobing();
   }
-}, 1000);
+});
+body.prepend(strobeButton);
+
+function startStrobing() {
+  strobeInterval = setInterval(() => {
+    let isLight = document.documentElement.classList.contains("light-theme");
+    if (isLight) {
+      document.documentElement.classList.remove("light-theme");
+      document.documentElement.classList.add("dark-theme");
+    } else {
+      document.documentElement.classList.remove("dark-theme");
+      document.documentElement.classList.add("light-theme");
+    }
+  }, 1000);
+}
+
+function stopStrobing() {
+  clearInterval(strobeInterval);
+}
