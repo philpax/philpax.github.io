bdb0f7ddf6c658535ae32ab4e10da757f596d345
Philpax <me@philpax.me>
2024-04-18 23:18:34 +0200

    feat: cobble together a basic style

--- src/styles/mod.rs
+++ src/styles/mod.rs
@@ -2,12 +2,16 @@ const RESET: &str = include_str!("reset.css");
 
 const DARK_MODE: &str = r#"
 background-image: url('/backgrounds/dark/20190902_225741.jpg');
-color: white;
+
+--color: white;
+--background-color: rgba(0, 0, 0, 0.4);
 "#;
 
 const LIGHT_MODE: &str = r#"
 background-image: url('/backgrounds/light/20230610_105204.jpg');
-color: black;
+
+--color: black;
+--background-color: rgba(255, 255, 255, 0.4);
 "#;
 
 const WEBSITE: &str = include_str!("website.css");
