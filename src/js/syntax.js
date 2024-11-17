e8425ee2f1aa2b9398029e87946d155b2337e20f
Philpax <me@philpax.me>
2024-11-17 02:21:02 +0100

    fix(syntax): remember setting

--- src/js/syntax.js
+++ src/js/syntax.js
@@ -4,9 +4,27 @@ document.addEventListener("DOMContentLoaded", function () {
     return;
   }
 
+  // Load theme from cookie if exists
+  const savedTheme = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("syntax-theme="));
+  if (savedTheme) {
+    const theme = savedTheme.split("=")[1];
+    selector.value = theme;
+    const syntaxTheme = document.getElementById("syntax-theme");
+    syntaxTheme.href = `/syntax/${theme}.css`;
+  }
+
   selector.style.visibility = "visible";
   selector.addEventListener("change", function () {
     const syntaxTheme = document.getElementById("syntax-theme");
     syntaxTheme.href = `/syntax/${selector.value}.css`;
+
+    // Save theme selection in cookie that expires in 1 year
+    const oneYear = new Date();
+    oneYear.setFullYear(oneYear.getFullYear() + 1);
+    document.cookie = `syntax-theme=${
+      selector.value
+    };expires=${oneYear.toUTCString()};path=/`;
   });
 });
