2f2953269b64c1d0004bc63523fb238cca529a23
Philpax <me@philpax.me>
2024-12-28 03:33:39 +0100

    feat: phosphor icon theme switcher

--- src/js/website.js
+++ src/js/website.js
@@ -4,32 +4,44 @@ function createThemeSwitcher() {
     console.log("Header links not found");
     return;
   }
-
   var li = document.createElement("li");
-  {
-    var a = document.createElement("a");
-    updateSwitcherInnerText(a, isLightMode());
-    a.href = "#";
+  var img = document.createElement("img");
+  updateSwitcherIcon(img, isLightMode());
+  img.style.cursor = "pointer";
 
-    a.addEventListener("click", function () {
-      var isLight = isLightMode();
-      if (isLight) {
-        document.documentElement.classList.remove("light-theme");
-        document.documentElement.classList.add("dark-theme");
-      } else {
-        document.documentElement.classList.remove("dark-theme");
-        document.documentElement.classList.add("light-theme");
-      }
-      updateSwitcherInnerText(a, !isLight);
-    });
+  img.addEventListener("click", function () {
+    var isLight = isLightMode();
+    if (isLight) {
+      document.documentElement.classList.remove("light-theme");
+      document.documentElement.classList.add("dark-theme");
+    } else {
+      document.documentElement.classList.remove("dark-theme");
+      document.documentElement.classList.add("light-theme");
+    }
+    updateSwitcherIcon(img, !isLight);
+  });
 
-    li.appendChild(a);
-  }
-
+  li.appendChild(img);
   headerLinks.append(li);
 
-  function updateSwitcherInnerText(a, isLightMode) {
-    a.innerText = isLightMode ? "Light Mode" : "Dark Mode";
+  function updateSwitcherIcon(img, isLightMode) {
+    // Get the SVG content and update its fill color
+    fetch(isLightMode ? "/phosphor/sun.svg" : "/phosphor/moon.svg")
+      .then((response) => response.text())
+      .then((svgText) => {
+        const parser = new DOMParser();
+        const svgDoc = parser.parseFromString(svgText, "image/svg+xml");
+        const svgElement = svgDoc.querySelector("svg");
+        const computedStyle = getComputedStyle(document.documentElement);
+        svgElement.style.fill =
+          computedStyle.getPropertyValue("--background-color");
+
+        const serializer = new XMLSerializer();
+        const svgString = serializer.serializeToString(svgDoc);
+        const svgUrl = "data:image/svg+xml;base64," + btoa(svgString);
+        img.src = svgUrl;
+        img.alt = isLightMode ? "Light Mode" : "Dark Mode";
+      });
   }
 
   function isLightMode() {
