1e85bc480e6f408928dbdf7d38c3a3b407bd4f5b
Philpax <me@philpax.me>
2026-06-04 06:37:29 +0200

    feat: restore ToC style

--- src/views/posts/mod.rs
+++ src/views/posts/mod.rs
@@ -253,7 +253,7 @@ pub fn toc_elements<'a>(
     bump: &'a Bump,
     toc: Option<paxhtml::Element<'a>>,
 ) -> (Option<paxhtml::Element<'a>>, Option<paxhtml::Element<'a>>) {
-    let h3_classname = format!("text-sm font-bold text-phosphor mb-1 {CODE_FONT_STYLE}");
+    let h3_classname = format!("font-bold text-phosphor mb-1");
     let link_classes = "toc [&_a]:text-dim [&_a]:no-underline [&_a:hover]:text-hot [&_a.active]:bg-phosphor [&_a.active]:text-canvas [&_a.active]:font-bold [&_a.active]:rounded-sm [&_a.active]:px-1 [&_a.active]:-mx-1";
     let toc_header = "Table of Contents";
 
